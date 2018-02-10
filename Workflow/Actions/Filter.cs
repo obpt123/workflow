603af032d6bc073f1c2bf1a49ff9d7fef202b0bb
杨鹏博 <obpt@sina.com>
2018-02-10 22:59:09 +0800

    add while ,filter

---
+++ Workflow/Actions/Filter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Workflows.Workflow.Actions
+{
+    public class Filter : IAction
+    {
+        public IEnumerable Source { get; set; }
+        public string Condition { get; set; }
+        public ActionResult Exec(IActionContext context)
+        {
+            return null;
+        }
+    }
+}
---
+++ Workflow/Actions/While.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Workflows.Workflow.Actions
+{
+    public class While : IAction, IChainEntry
+    {
+        public ActionChain Entry
+        {
+            get;set;
+        }
+
+        public string Condition { get; set; }
+
+        public ActionResult Exec(IActionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
